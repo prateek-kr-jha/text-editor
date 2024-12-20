3bfe417bbdb8283ef3abbe832a4f91d5301c8567
Prateek <prateekj1211@gmail.com>
2024-12-21 01:24:08 +0530

    added function to disable echo in terminal while typing

--- editorInCpp/kilo.cpp
+++ editorInCpp/kilo.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <termios.h>
+#include <unistd.h>
 
 
 void enableRawMode() {
+    struct termios raw;
 
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     // printf("Hello, World!\n");
     char c;
+    enableRawMode();
+    std::cout << STDIN_FILENO << "\n";
     while (std::cin.get(c) && c != 'q') {
-        // std::cout << c;
+        std::cout << c;
     }
     return 0;
 }
